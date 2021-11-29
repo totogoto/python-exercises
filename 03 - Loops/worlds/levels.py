c62fa48e7530f540f37c7a569600063abb6993a0
azad <indresh@sixerclass.com>
2021-11-29 12:53:17 +0530

    rearrange

--- 03 - Loops/worlds/levels.py
+++ 03 - Loops/worlds/levels.py
@@ -17,77 +17,25 @@ def jump_loop(world):
         world.add_object(x+2, 1, 'star', 1 )
         world.add_pick_obj_goal(x+2, 1, 'star', 1 )
 
-def move_i(world):
-    star_pos = [3,6,10,15]
-    for x in range(2,16):
-        if x in star_pos:
-            world.add_object(x, 1, 'star', 1)
-            world.add_pick_obj_goal(x, 1, 'star', 1)
-        else:
-            world.add_flag(x, 1)
-    world.add_flag_count_goal(10)
-            
-def move_spiral(world):
-    for i in range(5):
-        world.add_flag(i +2,1)
-    world.add_object(7, 1, 'star', 1)
-    world.add_pick_obj_goal(7, 1, 'star', 1)
-
-    for i in range(4):
-        world.add_flag(7,i+2)
-    world.add_object(7, 6, 'star', 1)
-    world.add_pick_obj_goal(7, 6, 'star', 1)
-    
-    for i in range(3):
-        world.add_flag(i +4,6)
-    world.add_object(3, 6, 'star', 1)
-    world.add_pick_obj_goal(3, 6, 'star', 1)
-    for i in range(2):
-        world.add_flag(3,i+4)
-    world.add_object(3, 3, 'star', 1)
-    world.add_pick_obj_goal(3, 3, 'star', 1)
-    world.add_object(5, 3, 'star', 1)
-    world.add_pick_obj_goal(5, 3, 'star', 1)
-    world.add_flag(4,3)
-    world.add_flag_count_goal(15)
-
-def triangle_5(world):
-    for x in range(1,6):
-        for i in range(x):
-            world.add_flag(i+1,x+1)
-        world.add_object(x+1, x+1, 'star', 1 )
-        world.add_pick_obj_goal(x+1, x+1, 'star', 1 )
-    world.add_flag_count_goal(15)
-
-def triangle_odd(world):
-    for x in range(1,11,2):
-        world.add_object(x+2, x+2, 'star', 1 )
-        world.add_pick_obj_goal(x+2, x+2, 'star', 1 )
         
 def take_5_and_put_5(world):
     for x in range(5):
         world.add_object(x+2, 1, 'star',1)
-    world.add_drop_obj_goal(7, 1, 'star',5)    
+    world.add_drop_obj_goal(7, 1, 'star',5)
 
-def take_loop(world):
-    for x in range(5):
-        world.add_object(x+2, 1, 'star',x+1)
-    world.add_drop_obj_goal(7, 1, 'star',15) 
-    
+def pick_star(world):
+    world.add_pick_obj_goal(6,1, "star", 10)
 
-def take_odd(world):
-    for x in range(5, 11):
-        world.add_object(x-3, 1, 'star',x)
-    world.add_drop_obj_goal(8, 1, 'star',45) 
+def pick_flowers(world):
+    world.add_pick_obj_goal(2,1, "daisy", 10)
+    world.add_pick_obj_goal(4,1, "daisy", 10)
+    world.add_pick_obj_goal(3,1, "daisy", 10)
+    world.add_pick_obj_goal(5,1, "daisy", 10)
 
 get_robo = get_robo_builder(levels={
     'around_the_room': around_the_room,
     'jump_loop': jump_loop,
-    'move_i': move_i,
-    'move_spiral': move_spiral,
-    'triangle_5': triangle_5,
-    'triangle_odd': triangle_odd,
-    'take_5_and_put_5': take_5_and_put_5,
-    'take_loop': take_loop,
-    'take_odd': take_odd
+    'pick_star': pick_star,
+    'pick_flowers': pick_flowers,
+    'take_5_and_put_5': take_5_and_put_5
 })
